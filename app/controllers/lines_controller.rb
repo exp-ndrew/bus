92d77e2cfa51dd6453f8c9d887afde3123615f6f
Matthew Williams & Andrew M Westling <student@epicodus.com>
2014-09-02 14:09:23 -0700

    add 7 RESTful methods for Station model

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -17,5 +17,31 @@ class LinesController < ApplicationController
     end
   end
 
+  def show
+    @line = Line.find(params[:id])
+  end
+
+  def destroy
+    @line = Line.find(params[:id])
+    @line.destroy
+    flash[:alert] = "#{@line.name.capitalize} line deleted."
+    redirect_to lines_path
+  end
+
+  def edit
+    @line = Line.find(params[:id])
+  end
+
+  def update
+    @line = Line.find(params[:id])
+    old_name = @line.name
+    @line.update(params[:line])
+    if @line.save
+      flash[:notice] = "#{old_name} has been updated to #{@line.name}"
+      redirect_to line_path(@line)
+    else
+      render 'edit'
+    end
+  end
 
 end
--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,5 +1,47 @@
 class StationsController < ApplicationController
   def index
-
+    @stations = Station.all
   end
+
+  def new
+    @station = Station.new
+  end
+
+  def create
+    @station = Station.create(params[:station])
+    if @station.save
+      flash[:notice] = "Station created."
+      redirect_to stations_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @station = Station.find(params[:id])
+  end
+
+  def destroy
+    @station = Station.find(params[:id])
+    @station.destroy
+    flash[:alert] = "#{@station.name.capitalize} station deleted."
+    redirect_to stations_path
+  end
+
+  def edit
+    @station = Station.find(params[:id])
+  end
+
+  def update
+    @station = Station.find(params[:id])
+    old_name = @station.name
+    @station.update(params[:station])
+    if @station.save
+      flash[:notice] = "#{old_name} has been updated to #{@station.name}"
+      redirect_to station_path(@station)
+    else
+      render 'edit'
+    end
+  end
+
 end
